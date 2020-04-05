331a013315a96b8315ec490f4a794e18a0baf1db
Christopher Cid <iamcid17@gmail.com>
2020-04-05 03:25:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
       :equilateral 
     elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_3 == @side_2 
       :isosceles 
-    elsif @side_1 != @side_2 && @side_1 != @side_3 && @side_3 != @side_3 
+    elsif @side_1 != @side_2 && @side_1 != @side_3 && @side_2 != @side_3 
       :scalene 
     else
       raise TriangleError
